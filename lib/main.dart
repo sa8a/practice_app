599e36f0df9f6d9a6988dbf26b75dc41c7db467c
sa8a <jpkrllll@yahoo.co.jp>
2022-12-24 03:14:51 +0900

    add: トグルスイッチ

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:practice_app/first_page.dart';
-import 'package:practice_app/second_page.dart';
-import 'package:practice_app/third_page.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  final router = GoRouter(
-    // パス (アプリが起動したとき)
-    initialLocation: '/first',
-
-    // パスと画面の組み合わせ
-    routes: [
-      GoRoute(
-        path: '/first',
-        builder: (context, state) => const FirstPage(),
-      ),
-      GoRoute(
-        path: '/second',
-        builder: (context, state) => const SecondPage(),
-      ),
-      GoRoute(
-        path: '/third',
-        builder: (context, state) => const ThirdPage(),
-      ),
-    ],
-  );
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      // go_routerのおまじない
-      routeInformationProvider: router.routeInformationProvider,
-      routeInformationParser: router.routeInformationParser,
-      routerDelegate: router.routerDelegate,
+    return const MaterialApp(
+      // 開発中のSlow Modeバナーを非表示にする
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+// トグルスイッチ
+final isOnProvaider = StateProvider((ref) {
+  return true;
+});
+
+// スライダーの数値
+final valueProvider = StateProvider((ref) {
+  return 0.0;
+});
+
+// レンジスライダー
+final rangeProvider = StateProvider((ref) {
+  return const RangeValues(0.0, 1.0);
+});
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isOn = ref.watch(isOnProvaider);
+    final toggleSwitch = Switch(
+      value: isOn,
+      onChanged: (isOn) {
+        ref.read(isOnProvaider.notifier).state = isOn;
+      },
+      activeColor: Colors.blue,
+      activeTrackColor: Colors.green,
+      inactiveThumbColor: Colors.black,
+      inactiveTrackColor: Colors.grey,
+    );
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            toggleSwitch,
+          ],
+        ),
+      ),
     );
   }
 }
