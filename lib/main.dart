9d7f37d38b407f26fcc34d09222ce3f9689b349e
sa8a <jpkrllll@yahoo.co.jp>
2022-12-24 23:32:54 +0900

    非同期処理の簡単なサンプル

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  xxx();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      // 開発中のSlow Modeバナーを非表示にする
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
+xxx() async {
+  debugPrint('私は x です');
+  debugPrint('y を await します');
+  await yyy();
+  debugPrint('y を await しました');
 }
 
-// トグルスイッチ
-final isOnProvider = StateProvider((ref) {
-  return true;
-});
+Future yyy() async {
+  debugPrint('私は y です');
+  debugPrint('z を await します');
+  await zzz();
+  debugPrint('z を await しました');
+}
 
-// スライダーの数値
-final valueProvider = StateProvider((ref) {
-  return 0.0;
-});
-
-// レンジスライダー
-final rangeProvider = StateProvider((ref) {
-  return const RangeValues(0.0, 1.0);
-});
-
-class HomePage extends ConsumerWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // トグルスイッチ
-    final isOn = ref.watch(isOnProvider);
-    final toggleSwitch = Switch(
-      value: isOn,
-      onChanged: (isOn) {
-        ref.read(isOnProvider.notifier).state = isOn;
-      },
-      activeColor: Colors.blue,
-      activeTrackColor: Colors.green,
-      inactiveThumbColor: Colors.black,
-      inactiveTrackColor: Colors.grey,
-    );
-
-    // スライダー
-    final value = ref.watch(valueProvider);
-    final slider = Slider(
-      value: value,
-      onChanged: (value) {
-        ref.read(valueProvider.notifier).state = value;
-        print(value);
-      },
-      thumbColor: Colors.blue,
-      activeColor: Colors.green,
-      inactiveColor: Colors.black,
-    );
-
-    final redBox = Container(
-      color: Colors.red,
-      width: value * 300,
-      height: 50,
-    );
-
-    // レンジスライダー
-    final range = ref.watch(rangeProvider);
-    final rangeSlider = RangeSlider(
-      values: range,
-      onChanged: (range) {
-        ref.read(rangeProvider.notifier).state = range;
-        print(range);
-      },
-      activeColor: Colors.green,
-      inactiveColor: Colors.black,
-    );
-
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            toggleSwitch,
-            slider,
-            redBox,
-            rangeSlider,
-          ],
-        ),
-      ),
-    );
-  }
+Future zzz() {
+  debugPrint('私は z です');
+  debugPrint('通信中...しばらくお待ちください');
+  return Future.delayed(const Duration(seconds: 3));
 }
