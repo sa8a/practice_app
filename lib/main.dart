e81cb0c912fcf075b2ec022410544d095c6c9199
sa8a <jpkrllll@yahoo.co.jp>
2022-12-28 06:27:48 +0900

    `mateapp` コマンド

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  xxx();
-}
+void main() => runApp(const MyApp());
 
-xxx() async {
-  debugPrint('私は x です');
-  debugPrint('y を await します');
-  final data = await yyy();
-  debugPrint('y から受け取ったデータは $data');
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-Future<int> yyy() async {
-  debugPrint('私は y です');
-  debugPrint('z を await します');
-  final data = await zzz();
-  debugPrint('z から受け取ったデータは $data');
-  return data;
-}
-
-Future<int> zzz() {
-  debugPrint('私は z です');
-  debugPrint('通信中...しばらくお待ちください');
-  return Future.delayed(const Duration(seconds: 3), () => 777);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
 }
