79ab427bdd77afb4dcb175ddfde94d20389ed8d5
sa8a <jpkrllll@yahoo.co.jp>
2022-12-28 06:41:58 +0900

    copyWithで値を変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:practice_app/fish.dart';
 
-void main() => runApp(const MyApp());
+// メイン関数
+void main() {
+  const app = MaterialApp(home: Home());
+  const scope = ProviderScope(child: app);
+  runApp(scope);
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// 魚データの状態管理
+final fishProvider = StateProvider((ref) {
+  return const Fish(
+    name: 'マグロ',
+    size: 200,
+    price: 300,
+  );
+});
+
+// ホーム画面
+class Home extends ConsumerWidget {
+  const Home({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+  Widget build(BuildContext context, WidgetRef ref) {
+    // 魚データ
+    final fish = ref.watch(fishProvider);
+    // ABCリスト
+    // final abcList = ref.watch(abcListProvider);
+
+    // 名前テキスト
+    final nameText = Text(
+      '名前: ${fish.name}',
+    );
+
+    // 大きさテキスト
+    final sizeText = Text(
+      '大きさ: ${fish.size} cm',
+    );
+
+    // 値段テキスト
+    final priceText = Text(
+      '値段: ${fish.price} 万円',
+    );
+
+    // ボタン
+    final button = ElevatedButton(
+      onPressed: () => onPressed(ref),
+      child: const Text('変更する'),
+    );
+
+    // 縦に並べるカラム
+    final column = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        nameText,
+        sizeText,
+        priceText,
+        button,
+      ],
+    );
+
+    // 画面の真ん中にカラムを置く
+    return Scaffold(
+      body: Center(
+        child: column,
       ),
     );
+  }
+
+  // ボタンを押したときの関数
+  void onPressed(WidgetRef ref) {
+    // 今画面に出ている魚
+    final fish = ref.read(fishProvider);
+
+    // 入れ物ごと変えた 新しい魚
+    final newFish = fish.copyWith(
+      // 値段は 500 にする
+      price: 500,
+    );
+
+    // 画面を変更する
+    ref.read(fishProvider.notifier).state = newFish;
   }
 }
