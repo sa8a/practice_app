e4bf88d535daa9a62d856862d8d1e024d613dada
sa8a <jpkrllll@yahoo.co.jp>
2022-12-28 06:33:50 +0900

    add: Fishクラス

---
+++ lib/fish.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'fish.freezed.dart';
+
+@freezed
+class Fish with _$Fish {
+  const factory Fish({
+    // 名前
+    required String name,
+    // 大きさ
+    required int size,
+    // 値段
+    required int price,
+  }) = _Fish;
+}
